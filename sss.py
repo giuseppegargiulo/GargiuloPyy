27e0eb743fe1b8993e591b8eb67d61f29560b249
giuseppegargiulo <giuseppe.gargiulo.s4@liceoviconapoli.it>
2021-02-08 15:25:19 +0100

    delete

--- sss.py
+++
@@ -1,9 +0,0 @@
-def max_frequency(x):
-    for i in x:
-        n=0
-        for j in x:
-            if i == j :
-                n += 1
-                print("la lettera",i,"é ripetuta", n, "volte nella parola")
-x= input("inserisci una parola:")
-print(max_frequency(x))
